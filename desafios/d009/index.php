803bd5a36b3f34cd8e6fef8afb96d3b56e4d00af
Thiago Costa <tigurrock@gmail.com>
2023-04-26 19:05:43 -0300

    att

---
+++ desafios/d009/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+        $numero = $_GET['num'] ?? 1;
+    ?>
+    <main>
+        <h1>Médias aritméticas</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="num">Número</label>
+            <input type="number" name="num" id="num" value="<?=$numero?>">
+            <input type="submit" value="Calcular raízes">
+        </form>
+    </main>
+
+</body>
+</html>
